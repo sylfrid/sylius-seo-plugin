cd9dd421fab7e1162e1225093df47dea33825a6c
Joseph Bodard <jbodard@odiso.com>
2021-07-17 18:22:51 +0200

    SF5 compatibility

--- src/DataCollector/RichSnippetsDataCollector.php
+++ src/DataCollector/RichSnippetsDataCollector.php
@@ -19,7 +19,7 @@ class RichSnippetsDataCollector extends DataCollector
         $this->richSnippetContext = $richSnippetContext;
     }
 
-    public function collect(Request $request, Response $response)
+    public function collect(Request $request, Response $response, \Throwable $exception = null)
     {
         $this->data['rich_snippets'] = $this->richSnippetContext->getAvailableRichSnippets();
         $this->data['html'] = $response->getContent();
